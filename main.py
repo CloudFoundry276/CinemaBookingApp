eaa4863b643a42e2a81a275a60c348b011a79264
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-08 01:17:19 +0530

    complete code of app

--- main.py
+++ main.py
@@ -1,43 +1,155 @@
+from fpdf import FPDF
+import random
+import string
+import sqlite3
+
+
 class User:
+    """Represents a user that can buy a cinema Seat"""
     def __init__(self, name):
         self.name = name
 
     def buy(self, seat, card):
-        pass
+        """Buys the ticket if the card is valid"""
+        if seat.is_free():
+            if card.validate(price=seat.get_price()):
+                seat.occupy()
+                ticket = Ticket(user=self, price=seat.get_price(), seat_number=seat_id)
+                ticket.to_pdf()
+                return "Purchase successful!"
+            else:
+                return "There was a problem with your card!"
+        else:
+            return "Seat is taken!"
 
 
 class Seat:
-    def __init__(self, database, seat_id, price, availability):
-        self.database = database
+    """Represents a cinema seat that can be taken from a User"""
+    database = "cinema.db"
+
+    def __init__(self, seat_id):
         self.seat_id = seat_id
-        self.price = price
-        self.availability = availability
+
+    def get_price(self):
+        """Get the price of a certain seat"""
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "price" FROM "Seat" WHERE "seat_id" = ?
+        """, [self.seat_id])
+        price = cursor.fetchall()[0][0]
+        return price
 
     def is_free(self):
-        pass
+        """Check in the database if a seat is taken or not"""
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "taken" FROM "Seat" WHERE "seat_id" = ?
+        """, [self.seat_id])
+        result = cursor.fetchall()[0][0]
+
+        if result == 0:
+            return True
+        else:
+            return False
 
     def occupy(self):
-        pass
+        """Change value of taken in the database from 0 to 1 if Seat if free"""
+        if self.is_free():
+            connection = sqlite3.connect(self.database)
+            connection.execute("""
+            UPDATE "Seat" SET "taken" = ? WHERE "seat_id" = ?
+            """, [1, self.seat_id])
+            connection.commit()
+            connection.close()
 
 
 class Card:
-    def __init__(self, database, type, number, cvc, holder):
-        self.database = database
+    """Represents a bank card needed to finalize a Seat purchase"""
+    database = "banking.db"
+
+    def __init__(self, type, number, cvc, holder):
         self.type = type
         self.number = number
         self.cvc = cvc
         self.holder = holder
 
     def validate(self, price):
-        pass
+        """Checks if Card is valid and has balance.
+        Subtracts price from balance.
+        """
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "balance" FROM "Card" WHERE "number" = ? and "cvc" = ?
+        """, [self.number, self.cvc])
+        result = cursor.fetchall()
+
+        if result:
+            balance = result[0][0]
+            if balance >= price:
+                connection.execute("""
+                UPDATE "Card" SET "balance" = ? WHERE "number" = ? and "cvc" = ?
+                """, [balance - price, self.number, self.cvc])
+                connection.commit()
+                connection.close()
+                return True
 
 
 class Ticket:
-    def __init__(self, id, user, price, seat):
-        self.id = id
+    """Represents a cinema Ticket purchased by a User"""
+    def __init__(self, user, price, seat_number):
         self.user = user
         self.price = price
-        self.seat = seat
+        self.id = "".join([random.choice(string.ascii_letters) for i in range(8)])
+        self.seat_number = seat_number
 
-    def to_pdf(self, path):
-        pass
+    def to_pdf(self):
+        """Creates a PDF ticket"""
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.cell(w=0, h=80, txt="Your Digital Ticket", border=1, ln=1, align="C")
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Name: ", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=self.user.name, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Ticket ID: ", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=self.id, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Price: ", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=str(self.price), border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Seat Number: ", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=str(self.seat_number), border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.output("sample.pdf", 'F')
+
+
+if __name__ == "__main__":
+    name = input("Enter Your Full Name: ")
+    seat_id = input("Enter Preferred Seat Number: ")
+    card_type = input("Enter Your Card Type: ")
+    card_number = input("Enter Your Card Number: ")
+    card_cvc = input("Enter Card CVC Number: ")
+    card_holder = input("Enter Card Holder Name: ")
+
+    user = User(name=name)
+    seat = Seat(seat_id=seat_id)
+    card = Card(type=card_type, number=card_number, cvc=card_cvc, holder=card_holder)
+
+    print(user.buy(seat, card))
